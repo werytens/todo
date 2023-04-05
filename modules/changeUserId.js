1ac96ea3cd8a20b82168361bb6684105fa3b0263
Липатов Максим <103856799+greydatlenght@users.noreply.github.com>
2023-04-05 23:02:57 +0300

    Delete modules directory

--- modules/changeUserId.js
+++
@@ -1,7 +0,0 @@
-export function changeUserID(buttonName, names) {
-    for (let index = 0; index < names.length; index++) {
-        if (buttonName == names[index].name) {
-            return index
-        }
-    }  
-}
--- modules/clearingItems.js
+++
@@ -1,4 +0,0 @@
-export function clearingItems() {
-    const itemblocksArray = document.querySelectorAll(".itemblock")
-    itemblocksArray.forEach(element => element.remove())
-}
